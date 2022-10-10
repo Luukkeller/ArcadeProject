bde1f301ecf79d2eb0c3b91b69034c0a07480a69
markhekstra <mark.hekstra@student.nhlstenden.com>
2022-10-10 12:00:33 +0200

    MErge Fixes

--- arcadeGame/arcadeGame/GameWindow.xaml.cs
+++ arcadeGame/arcadeGame/GameWindow.xaml.cs
@@ -21,59 +21,63 @@ namespace arcadeGame
     /// </summary>
     public partial class GameWindow : Window
     {
-        bool moveLeft, moveRight, moveLeft2, moveRight2;
+        bool moveLeft1, moveRight1, moveLeft2, moveRight2;
         bool isPressed1 = false;
         bool isPressed2 = false;
         const int playerSpeed = 10;
         const int bulletSpeed = 10;
         private DispatcherTimer gameTimer = new DispatcherTimer();
+
         List<Rectangle> itemsToRemove = new List<Rectangle>();
+
         List<Rectangle> playerBullets = new List<Rectangle>();
-        ImageBrush playerSkin = new ImageBrush();
+
+        ImageBrush player1Skin = new ImageBrush();
         ImageBrush player2Skin = new ImageBrush();
 
         public GameWindow()
         {
             InitializeComponent();
 
-            gameTimer.Tick += GameLoop;
+            gameTimer.Tick += GameEngine;
             gameTimer.Interval = TimeSpan.FromMilliseconds(10);
             gameTimer.Start();
 
             // player 1 skin
-            playerSkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/assets/strangerThings1.png"));
-            player1.Fill = playerSkin;
+            player1Skin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/assets/strangerThings1.png"));
+            Player1.Fill = player1Skin;
 
             // player 2 skin
             player2Skin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/assets/strangerThings2.png"));
-            player2.Fill = player2Skin;
+            Player2.Fill = player2Skin;
 
-            Canvas.Focus();
+            MyCanvas.Focus();
         }
 
-        private void GameLoop(object sender, EventArgs e)
+        private void GameEngine(object sender, EventArgs e)
         {
             //Player 1
-            if (moveLeft == true && Canvas.GetLeft(player1) > 0)
+            if (moveLeft1 == true && Canvas.GetLeft(Player1) > 0)
             {
-                Canvas.SetLeft(player1, Canvas.GetLeft(player1) - playerSpeed);
+                Canvas.SetLeft(Player1, Canvas.GetLeft(Player1) - playerSpeed);
             }
-            if (moveRight == true && Canvas.GetLeft(player1) + 75 < Application.Current.MainWindow.Width)
+            if (moveRight2 == true && Canvas.GetLeft(Player1) + 75 < Application.Current.MainWindow.Width)
             {
-                Canvas.SetLeft(player1, Canvas.GetLeft(player1) + playerSpeed);
+                Canvas.SetLeft(Player1, Canvas.GetLeft(Player1) + playerSpeed);
             }
             // Player 2
-            if (moveLeft2 == true && Canvas.GetLeft(player2) > 0)
+            if (moveLeft2 == true && Canvas.GetLeft(Player2) > 0)
             {
-                Canvas.SetLeft(player2, Canvas.GetLeft(player2) - playerSpeed);
+                Canvas.SetLeft(Player2, Canvas.GetLeft(Player2) - playerSpeed);
             }
-            if (moveRight2 == true && Canvas.GetLeft(player2) + 75 < Application.Current.MainWindow.Width)
+            if (moveRight2 == true && Canvas.GetLeft(Player2) + 75 < Application.Current.MainWindow.Width)
             {
-                Canvas.SetLeft(player2, Canvas.GetLeft(player2) + playerSpeed);
+                Canvas.SetLeft(Player2, Canvas.GetLeft(Player2) + playerSpeed);
             }
+
             //player bullets logic
             //searches for all rectangles in Canvas
-            foreach (Rectangle x in Canvas.Children.OfType<Rectangle>())
+            foreach (Rectangle x in MyCanvas.Children.OfType<Rectangle>())
             {
                 //filters rectangles with bullet1, bullet2 tags
                 if ((string)x.Tag == "bullet1" || (string)x.Tag == "bullet2")
@@ -87,16 +91,16 @@ namespace arcadeGame
             }
         }
 
-        private void KeyPressed(object sender, KeyEventArgs e)
+        private void OnKeyDown(object sender, KeyEventArgs e)
         {
             //player 1 movement (left or right movement key is pressed)
             if (e.Key == Key.A)
             {
-                moveLeft = true;
+                moveLeft1 = true;
             }
             if (e.Key == Key.D)
             {
-                moveRight = true;
+                moveRight2 = true;
             }
             //player 2 movement (A or D movement key is pressed)
             if (e.Key == Key.Left)
@@ -107,7 +111,7 @@ namespace arcadeGame
             {
                 moveRight2 = true;
             }
-            //bullet player1
+            //bullet Player1
             if (e.Key == Key.W && !isPressed1)
             {
                 isPressed1 = true;
@@ -122,14 +126,14 @@ namespace arcadeGame
                 //adds player bullet to list
                 playerBullets.Add(bulletPlayer1);
                 //sets location of bullet above the player vertically
-                Canvas.SetTop(bulletPlayer1, Canvas.GetTop(player1) - bulletPlayer1.Height);
+                Canvas.SetTop(bulletPlayer1, Canvas.GetTop(Player1) - bulletPlayer1.Height);
                 //sets location of bullet above the player horizontally
-                Canvas.SetLeft(bulletPlayer1, Canvas.GetLeft(player1) + player1.Width / 2);
+                Canvas.SetLeft(bulletPlayer1, Canvas.GetLeft(Player1) + Player1.Width / 2);
                 //adds bullet to game
-                Canvas.Children.Add(bulletPlayer1);
+                MyCanvas.Children.Add(bulletPlayer1);
             }
 
-            //bullet player2
+            //bullet Player2
             if (e.Key == Key.Up && !isPressed2)
             {
                 isPressed2 = true;
@@ -141,24 +145,24 @@ namespace arcadeGame
                     Fill = Brushes.White,
                 };
                 playerBullets.Add(bulletPlayer2);
-                Canvas.SetTop(bulletPlayer2, Canvas.GetTop(player2) - bulletPlayer2.Height);
-                Canvas.SetLeft(bulletPlayer2, Canvas.GetLeft(player2) + player2.Width / 2);
-                Canvas.Children.Add(bulletPlayer2);
+                Canvas.SetTop(bulletPlayer2, Canvas.GetTop(Player2) - bulletPlayer2.Height);
+                Canvas.SetLeft(bulletPlayer2, Canvas.GetLeft(Player2) + Player2.Width / 2);
+                MyCanvas.Children.Add(bulletPlayer2);
             }
         }
 
 
 
-        private void KeyReleased(object sender, KeyEventArgs e)
+        private void OnKeyUp(object sender, KeyEventArgs e)
         {
             //player 1 movement key is released
             if (e.Key == Key.A)
             {
-                moveLeft = false;
+                moveLeft1 = false;
             }
             if (e.Key == Key.D)
             {
-                moveRight = false;
+                moveRight2 = false;
                 //player 2 movement key is released
             }
             if (e.Key == Key.Left)
