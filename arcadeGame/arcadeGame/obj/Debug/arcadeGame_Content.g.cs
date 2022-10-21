a1d129fe79a589be68d396da8ed81b89f2d0d725
Rws <ruwansoepboer@outlook.com>
2022-10-21 13:41:54 +0200

    Light colour correction.

--- arcadeGame/arcadeGame/obj/Debug/arcadeGame_Content.g.cs
+++ arcadeGame/arcadeGame/obj/Debug/arcadeGame_Content.g.cs
@@ -8,7 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/enemyhit.mp3")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/music.mp3")]
 
 
